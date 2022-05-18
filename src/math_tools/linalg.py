af488e3a73ef8e702ae53ba1679151d56596a395
YangWu1227 <yangwu2020@gmail.com>
2022-05-18 01:39:18 -0400

    feat: add lin_ind function

--- src/math_tools/linalg.py
+++ src/math_tools/linalg.py
@@ -1,57 +1,102 @@
+# ---------------------------------------------------------------------------- #
+#                              Package and Modules                             #
+# ---------------------------------------------------------------------------- #
+
 import numpy as np
 import numpy.linalg as la
+from sympy import Matrix
+
+# ----------------------------- Standard library ----------------------------- #
+
+from typing import Union, List, Tuple
 
 # --------------- Express vector in terms of new basis vectors --------------- #
 
 
-def cob(vector: np.array, *args: np.array) -> np.array:
+def cob(vector: Union[np.ndarray, List, Tuple], *args: Union[np.ndarray, List, Tuple]) -> np.ndarray:
     """
-    Express a vector in terms of an orthogonal (not orthonomal) basis.
+    Express a vector in a new basis spanned by a pairwise orthogonal (not orthonomal) set basis vectors.
 
     Parameters
     ----------
-    vector : np.array
+    vector : Union[np.ndarray, List, Tuple]
         An n-dimensional vector.
-    args : np.array
-        An arbitrary number of n-dimensional vector.
+    args : Union[np.ndarray, List, Tuple]
+        An arbitrary number of pairwise orthogonal n-dimensional vectors.
 
     Returns
     -------
-    np.array
+    np.ndarray
         The original vector expressed in a new basis.
     """
-    scalar_proj = [np.dot(vector, basis) / (la.norm(basis))
-                   ** 2 for basis in args]
-    return np.array(scalar_proj)
+    # Cast to np.ndarray
+    vector = np.array(vector)
+    # Projection
+    scalar_projections = [np.dot(vector, np.array(basis)) / (la.norm(np.array(basis)))
+                          ** 2 for basis in args]
+    return np.array(scalar_projections)
 
-# --------------- Vector projection of one vector onto another --------------- #
+# ---------- Scalar or vector projection of one vector onto another ---------- #
 
 
-def proj(v1: np.array, v2: np.array, type='vector'):
+def proj(v1: Union[np.ndarray, List, Tuple], v2: Union[np.ndarray, List, Tuple], proj_type: str = 'vector') -> Union[np.ndarray, np.float64]:
     """
-    Obtain the vector or scalar project of vector `v1` onto vector `v2`.
+    Obtain the scalar or vector projection of vector `v1` onto vector `v2`.
 
     Parameters
     ----------
-    v1 : np.array
+    v1 : Union[np.ndarray, List, Tuple]
         An n-dimensional vector.
-    v2 : np.array
+    v2 : Union[np.ndarray, List, Tuple]
         An n-dimensional vector.
+    proj_type : str, optional
+        Type of projection, by default 'vector'.
+
 
     Returns
     -------
-    np.array
-        The scalar or vector project of vector `v1` onto vector `v2`.
+    np.ndarray
+        The scalar or vector projection of vector `v1` onto vector `v2`.
     """
+    v1 = np.array(v1)
+    v2 = np.array(v2)
 
-    if (type == 'scalar'):
-        out = (np.dot(v1, v2) / (la.norm(v2)) ** 2)
-    elif (type == 'vector'):
+    if (proj_type == 'scalar'):
+        out = (np.dot(v1, v2) / (la.norm(v2)))
+    elif (proj_type == 'vector'):
         out = (np.dot(v1, v2) / (la.norm(v2)) ** 2) * v2
     else:
-        ValueError("'type' must either be 'vector' or 'scalar")
+        raise ValueError("'proj_type' must either be 'vector' or 'scalar")
 
     return out
+
+# ----------------------- Check for linear independence ---------------------- #
+
+
+def lin_ind(*args: Union[np.ndarray, List, Tuple]) -> Union[None, np.ndarray]:
+    """
+    Check if the input vectors are linearly independent. If not, return the redundant
+    column vectors as an `np.ndarray`.
+
+    Returns
+    -------
+    None or np.ndarray
+        An `np.ndarray` of redundant columns or None if the input vectors are linearly independent.
+    """
+    # Convert input tuple of vectors into an ndarray for indexing later
+    args = np.array(args)
+
+    # Need to transpose the matrix so each vector in 'args' is a column vector
+    # Pivot column indices are stored as the second element of the tuple returned by rref()
+    pivot_indices = Matrix(args).T.rref()[1]
+
+    # If number of  elements in 'pivot_indices' equals the number of input vectors, then the matrix has full rank
+    if (len(pivot_indices) == args.shape[0]):
+        print("The matrix with input vectors as columns is full column rank, and so the input vectors are linearly independent")
+        return None
+    else:
+        # Return non-pivot columns
+        return np.delete(args, pivot_indices, axis=0)
 
 
 if __name__ == '__main__':
@@ -93,10 +138,21 @@ if __name__ == '__main__':
         np.array([-3, -6, 5])
     )
 
-    # ------------------------------ tests for proj ------------------------------ #
+    # ------------------------------ Tests for proj ------------------------------ #
 
     # One
-    proj(np.array([1, 2]), np.array([1, 1]))
+    proj(np.array([2, 4, 0]), np.array([4, 2, 4]), 'scalar')
 
     # Two
-    proj(np.array([2, 1]), np.array([3, -4]))
+    proj(np.array([2, 1]), np.array([3, -4]), 'scalar')
+
+    # ---------------------------- Tests for lin_indep --------------------------- #
+
+    # Linearly independent inputs
+    lin_ind([1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1], [0, 1, 0, 0])
+
+    # Linearly dependent inputs
+    lin_ind([1, 0, 0, 0], [1, 9, 3, 0], [
+        0, 0, 4, 1], [0, 1, 0, 0], [2, 3, 4, 9])
+
+    lin_ind([1, 2], [2, 4], [2, 1])
