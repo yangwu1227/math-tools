5cabf04dabd8da21e16fca12130bbebaf5684545
YangWu1227 <yangwu2020@gmail.com>
2024-08-11 02:31:06 -0500

    test: update tests

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -1,18 +1,5 @@
-# ---------------------------------------------------------------------------- #
-#                           Load packages and modules                          #
-# ---------------------------------------------------------------------------- #
-
 import numpy as np
-from scipy.stats import ortho_group
-import pytest
-
-# ------------------------------- Intra-package ------------------------------ #
-
 import math_tools.linalg as la
-
-# ---------------------------------------------------------------------------- #
-#                            Tests for cob function                            #
-# ---------------------------------------------------------------------------- #
 
 
 def test_cob():
@@ -21,36 +8,32 @@ def test_cob():
     """
     # Test 1:
     new_vec = la.cob(np.array([10, -5]), np.array([3, 4]), np.array([4, -3]))
-    assert la.MyMatrix.is_coord_vec(np.array([10, -5]), new_vec,
-                                    np.array([3, 4]), np.array([4, -3]))
+    assert la.MyMatrix.is_coord_vec(
+        np.array([10, -5]), new_vec, np.array([3, 4]), np.array([4, -3])
+    )
 
     # Test 2:
-    new_vec = la.cob(
+    new_vec = la.cob((1, 1, 1), (2, 1, 0), (1, -2, -1), (-1, 2, -5))
+    assert la.MyMatrix.is_coord_vec(
         (1, 1, 1),
-        (2, 1, 0),
-        (1, -2, -1),
-        (-1, 2, -5)
+        new_vec,
+        np.array([2, 1, 0]),
+        np.array([1, -2, -1]),
+        np.array([-1, 2, -5]),
     )
-    assert la.MyMatrix.is_coord_vec((1, 1, 1), new_vec,
-                                    np.array([2, 1, 0]), np.array([1, -2, -1]),
-                                    np.array([-1, 2, -5]))
 
     # Test 3:
     new_vec = la.cob(
-        [1, 1, 2, 3],
-        [1, 0, 0, 0],
-        [0, 2, -1, 0],
-        [0, 1, 2, 0],
-        [0, 0, 0, 3]
+        [1, 1, 2, 3], [1, 0, 0, 0], [0, 2, -1, 0], [0, 1, 2, 0], [0, 0, 0, 3]
     )
-    assert la.MyMatrix.is_coord_vec([1, 1, 2, 3], new_vec,
-                                    np.array([1, 0, 0, 0]), np.array(
-                                        [0, 2, -1, 0]),
-                                    np.array([0, 1, 2, 0]), np.array([0, 0, 0, 3]))
-
-# ---------------------------------------------------------------------------- #
-#                            Tests for proj function                           #
-# ---------------------------------------------------------------------------- #
+    assert la.MyMatrix.is_coord_vec(
+        [1, 1, 2, 3],
+        new_vec,
+        np.array([1, 0, 0, 0]),
+        np.array([0, 2, -1, 0]),
+        np.array([0, 1, 2, 0]),
+        np.array([0, 0, 0, 3]),
+    )
 
 
 def test_proj():
@@ -60,29 +43,24 @@ def test_proj():
 
     # Test 1:
     assert isinstance(
-        la.proj(np.array([2, 4, 0]), np.array([4, 2, 4]), 'scalar'),
-        np.float64
+        la.proj(np.array([2, 4, 0]), np.array([4, 2, 4]), "scalar"), np.float64
     )
 
     # Test 2:
     assert isinstance(
-        la.proj(np.array([2, 1]), np.array([3, -4]), 'vector'),
-        np.ndarray
+        la.proj(np.array([2, 1]), np.array([3, -4]), "vector"), np.ndarray
     )
 
     # Test 3:
-    assert isinstance(la.proj([2, 1, 5, 6, 10, 5],
-                              (3, -4, 0, 3, 0, 0), 'vector'),
-                      np.ndarray)
+    assert isinstance(
+        la.proj([2, 1, 5, 6, 10, 5], (3, -4, 0, 3, 0, 0), "vector"), np.ndarray
+    )
 
     # Test 4:
-    assert isinstance(la.proj([2, 1, 5, 6, 10, 5],
-                              (3, -4, 0, 3, 0, 0), 'scalar'), np.float64)
+    assert isinstance(
+        la.proj([2, 1, 5, 6, 10, 5], (3, -4, 0, 3, 0, 0), "scalar"), np.float64
+    )
 
-
-# ---------------------------------------------------------------------------- #
-#                          Tests for lin_ind function                          #
-# ---------------------------------------------------------------------------- #
 
 def test_lin_ind(capfd):
     """
@@ -92,52 +70,27 @@ def test_lin_ind(capfd):
     # Test 1 (Linear independent)
     la.lin_ind([1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1], [0, 1, 0, 0])
     captured = capfd.readouterr()
-    assert captured.out == "The matrix with input vectors as columns has full column rank, and so the input vectors are linearly independent\n"
+    assert (
+        captured.out
+        == "The matrix with input vectors as columns has full column rank, and so the input vectors are linearly independent\n"
+    )
 
     # Test 2 (Linear dependent)
-    assert np.allclose(la.lin_ind([1, 0, 0, 0], [1, 9, 3, 0], [0, 0, 4, 1], [
-                       0, 1, 0, 0], [2, 3, 4, 9]), np.array([[2, 3, 4, 9]]))
+    assert np.allclose(
+        la.lin_ind(
+            [1, 0, 0, 0], [1, 9, 3, 0], [0, 0, 4, 1], [0, 1, 0, 0], [2, 3, 4, 9]
+        ),
+        np.array([[2, 3, 4, 9]]),
+    )
 
     # Test 3
     assert np.allclose(la.lin_ind([1, 2], [2, 4], [2, 1]), np.array([[2, 4]]))
-
-# ---------------------------------------------------------------------------- #
-#                           Tests for MyMatrix class                           #
-# ---------------------------------------------------------------------------- #
 
 
 class TestMyMatrix:
     """
     Tests for MyMatrix class.
     """
-
-    # ---------------------- Tests for default instantiation --------------------- #
-
-    @pytest.mark.parametrize(
-        "data, axis, expected",
-        [
-            # List of lists
-            ([[1, 2], [1, 2]], 1, np.array([[1, 1], [2, 2]])),
-            # Tuple of tuple
-            (((1, 2), [1, 2]), 0, np.array([[1, 2], [1, 2]])),
-            # Non-square matrices
-            ([[1, 2, 3], [1, 2, 4]], 1, np.stack([[1, 2, 3], [1, 2, 4]], axis=1)),
-            ([[1, 2], [1, 2], [7, 7]], 1, np.stack(
-                [[1, 2], [1, 2], [7, 7]], axis=1))
-        ],
-        scope='function'
-    )
-    def test_default(self, data, axis, expected):
-        """
-        Test default instantiation.
-        """
-
-        assert np.allclose(la.MyMatrix.default(data, axis).X, expected)
-
-        assert hasattr(la.MyMatrix.default(data, axis), 'X')
-        assert hasattr(la.MyMatrix.default(data, axis), 'det')
-        assert hasattr(la.MyMatrix.default(data, axis), 'row')
-        assert hasattr(la.MyMatrix.default(data, axis), 'col')
 
     # ----------------- Tests for change of basis matrix function ---------------- #
 
@@ -149,17 +102,23 @@ class TestMyMatrix:
         # Test 1:
         assert np.allclose(
             # Change of basis from [[1, 2, -3], [4, -1, -3]] to [[0, 1, -1], [1, 0, -1]]
-            la.MyMatrix.cob_mat([[1, 2, -3], [4, -1, -3]],
-                                [[0, 1, -1], [1, 0, -1]], False),
+            la.MyMatrix.cob_mat(
+                [[1, 2, -3], [4, -1, -3]], [[0, 1, -1], [1, 0, -1]], False
+            ),
             # Inverse of change of basis matrix above
-            np.linalg.inv(la.MyMatrix.cob_mat([[1, 2, -3], [4, -1, -3]],
-                                              [[0, 1, -1], [1, 0, -1]], True))
+            np.linalg.inv(
+                la.MyMatrix.cob_mat(
+                    [[1, 2, -3], [4, -1, -3]], [[0, 1, -1], [1, 0, -1]], True
+                )
+            ),
         )
 
         # Test 2: Change of basis matrix from standard basis to an arbitrary basis
-        mat = la.MyMatrix.cob_mat([[1, 0, 0], [0, 1, 0], [0, 0, 1]],
-                                  [[2, 1, 0], [1, -2, -1], [-1, 2, -5]],
-                                  False)
+        mat = la.MyMatrix.cob_mat(
+            [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+            [[2, 1, 0], [1, -2, -1], [-1, 2, -5]],
+            False,
+        )
         # This should be equal
         np.allclose(
             # The change of basis matrix should translate [1, 1, 1] from standard basis to new basis
@@ -169,6 +128,6 @@ class TestMyMatrix:
                 np.array([1, 1, 1]),
                 np.array([2, 1, 0]),
                 np.array([1, -2, -1]),
-                np.array([-1, 2, -5])
-            )
+                np.array([-1, 2, -5]),
+            ),
         )
